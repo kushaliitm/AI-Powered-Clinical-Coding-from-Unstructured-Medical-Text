18da4bedf3dd7233bd782210379c7a70a8534fc8
kushaliitm <patelkushal96@gmail.com>
2026-02-18 21:07:24 -0800

    Changed file structure and added main files

--- agents/base_agent.py
+++
@@ -1,9 +0,0 @@
-class BaseAgent:
-    def __init__(self, name: str):
-        self.name = name
-
-    def load_model(self):
-        raise NotImplementedError("Must override load_model()")
-
-    def respond(self, *args, **kwargs):
-        raise NotImplementedError("Must override respond()")
